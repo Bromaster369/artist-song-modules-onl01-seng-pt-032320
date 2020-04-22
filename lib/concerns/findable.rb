cdf752f63343d1e326f32171b5037cadffa1fd58
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-22 00:58:52 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,11 @@
 module Findable
   
  def self.find_by_name(name)
-    self.detect{|a| a.name == name}
+    self.class.all.detect{|a| a.name == name}
   end
   
   def self.find_by_name(name)
-    self.detect{|a| a.name == name}
+    self.class.all.detect{|a| a.name == name}
   end
   
 end
