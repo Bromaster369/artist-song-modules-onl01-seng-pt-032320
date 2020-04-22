12bd552ee135d4e4466696696dd00d6b4713f384
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-22 01:00:58 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,11 @@
 module Findable
   
  def self.find_by_name(name)
-    self.class.all.detect{|a| a.name == name}
+    @@artists.detect{|a| a.name == name}
   end
   
   def self.find_by_name(name)
-    self.class.all.detect{|a| a.name == name}
+    @@songs.detect{|a| a.name == name}
   end
   
 end
