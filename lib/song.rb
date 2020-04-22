8a1450d8b99d7be19bdaf836df724f595d241672
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-22 00:53:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,7 @@ class Song
     @@songs << self
   end
 
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
+
 
   def self.all
     @@songs
@@ -30,7 +28,5 @@ class Song
     @artist = artist
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
+ 
 end
